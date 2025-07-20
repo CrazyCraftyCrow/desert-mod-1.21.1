ace105e5b801ea466c7b4276d28649c62aca42af
samim <sami.michalek@gmail.com>
2025-07-20 14:08:51 +0200

    Ancient Forge (finally) plus frog spawn fix

--- src/main/java/net/samuel/testdesertmod/world/gen/ModEntitySpawns.java
+++ src/main/java/net/samuel/testdesertmod/world/gen/ModEntitySpawns.java
@@ -13,7 +13,7 @@ import net.samuel.testdesertmod.entity.custom.DesertRainFrogEntity;
 public class ModEntitySpawns {
     public static void addSpawns() {
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DESERT),
-                SpawnGroup.CREATURE, ModEntities.DESERT_RAIN_FROG, 30,1,2);
+                SpawnGroup.AMBIENT, ModEntities.DESERT_RAIN_FROG, 20,1,2);
 
         SpawnRestriction.register(ModEntities.DESERT_RAIN_FROG, SpawnLocationTypes.ON_GROUND,
                 Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, DesertRainFrogEntity::canSpawn);
